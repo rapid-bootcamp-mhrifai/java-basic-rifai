e686e9a2af8ac51632bbc55c217e45efa184aff2
mhrifai <mkhrifai99@gmail.com>
2022-12-12 11:04:21 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        myFavoritFood();
+        myFavoritPlace();
+    }
+
+    public static void myFavoritFood(){
+        System.out.println("My Favorite Food;");
+        System.out.println("1. Ikan Bakar");
+        System.out.println("2. Ikan goreng");
+    }
+
+    public static void myFavoritPlace(){
+        System.out.println("My Favorite Place;");
+        System.out.println("1. Bali");
+        System.out.println("2. Central Java");
+    }
+
+}
